5f3e485dcf07a54079e501e453920e5adb9cce2c
Denis Oleschenko <denisoleshenko@gmail.com>
2019-09-26 18:08:52 +0300

    done

--- index.js
+++ index.js
@@ -14,27 +14,24 @@ const server = http.createServer((req, res) => {
 		fieldName = findParam.has('fieldName') ? findParam.get('fieldName') : 'default';
 		const compiledFunction = pug.compileFile('./views/form.pug');
 		res.write(compiledFunction({fieldName: fieldName}));
+		res.end();
 	} else if(req.method === 'POST' && req.url === '/') {
 		let body = '';
 		req.on('data', (data) => {
             body += data;
             // if we are getting too much POST data, kill the connection!
             // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
-            if (body.length > 1e6) req.connection.destroy();
+			if (body.length > 1e6) req.connection.destroy();
 		});
-		let post = qs.parse(body);
-		console.log(post[fieldName]);
-		console.log('fieldName:', fieldName);
-		const compiledFunction = pug.compileFile('./views/result_template.pug');
-        req.on('end', function () {
-            
-			// res.write(compiledFunction({fieldName: fieldName, value: post[fieldName]}));
+        req.on('end', () => {
+            let post = qs.parse(body);
+			const compiledFunction = pug.compileFile('./views/result_template.pug');
+			res.write(compiledFunction({fieldName: fieldName, value: post[fieldName]}));
+			res.end();
         });
 	} else {
 		res.write(`wrong request`);
 	}
-	res.end();
-	
 });
 
 server.listen(3000, (err) => {
